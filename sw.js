68b0f6e01cb069968eceff8e647b4a85c047c17d
Vadim <vadim-perog@mail.ru>
2026-08-20 19:25:51 +0300

    Неполный эквадорский короб, состав мест в заявке

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * худший вид ошибки — воспроизводится только у одного человека.
  */
 
-const ВЕРСИЯ = "florsbor-v8";
+const ВЕРСИЯ = "florsbor-v9";
 const ОФЛАЙН = new Request("./", { cache: "reload" });
 
 self.addEventListener("install", (e) => {
